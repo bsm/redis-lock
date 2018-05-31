c6e95f835468c94afaa8d18c53aa6119a601afe9
ifishjin <ifishjin@tencent.com>
2018-05-31 20:24:24 +0800

    add token prefix test case

--- lock_test.go
+++ lock_test.go
@@ -70,7 +70,7 @@ var _ = Describe("Locker", func() {
 		Expect(subject.Lock()).To(BeTrue())
 		Expect(subject.IsLocked()).To(BeTrue())
 
-		Expect(redisClient.Get(testRedisKey).Result()).To(HaveLen(24))
+		Expect(redisClient.Get(testRedisKey).Result()).To(HaveLen(24 + len(subject.opts.TokenPrefix)))
 		Expect(getTTL()).To(BeNumerically("~", time.Second, 10*time.Millisecond))
 	})
 
@@ -82,7 +82,7 @@ var _ = Describe("Locker", func() {
 		Expect(subject.IsLocked()).To(BeTrue())
 
 		Expect(redisClient.Get(testRedisKey).Result()).To(Equal(subject.token))
-		Expect(subject.token).To(HaveLen(24))
+		Expect(subject.opts.TokenPrefix).To(Equal(subject.token[:len(subject.token)-24]))
 		Expect(getTTL()).To(BeNumerically("~", time.Second, 10*time.Millisecond))
 	})
 
